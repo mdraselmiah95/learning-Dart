9d911e12dd48263eb39c53116e07009ca0ab5fef
mdraselmiah95 <russelroy15@gmail.com>
2026-06-14 18:55:03 +0600

    Learing Dart

--- practice/index.dart
+++ practice/index.dart
@@ -5,4 +5,20 @@ void main() {
   int secondsLeft = seconds % 60;
 
   print('Hours: $hours, Minutes: $minutes, Seconds: $secondsLeft');
+
+  // switch — clean exact matching
+  String status = "success";
+
+  switch (status) {
+    case "loading":
+      print("Loading...");
+      break;
+    case "success":
+      print("Data loaded successfully!");
+      break;
+    case "error":
+      print("An error occurred while loading data.");
+      break;
+    default:
+  }
 }

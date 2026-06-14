6bcdab72a221a68bf3b6325c4b039f330dca3315
mdraselmiah95 <russelroy15@gmail.com>
2026-06-14 18:58:32 +0600

    Learing Dart

--- practice/index.dart
+++ practice/index.dart
@@ -1,3 +1,13 @@
+void checkDrivingEligibility(int age, bool hasLicense) {
+  if (age >= 18 && hasLicense) {
+    print('Can drive');
+  } else if (age >= 18 && !hasLicense) {
+    print('Get a license first');
+  } else {
+    print('Too young');
+  }
+}
+
 void main() {
   int seconds = 3750;
   int hours = seconds ~/ 3600;
@@ -5,6 +15,24 @@ void main() {
   int secondsLeft = seconds % 60;
 
   print('Hours: $hours, Minutes: $minutes, Seconds: $secondsLeft');
+
+  // if / else if / else
+
+  int score = 85;
+  if (score >= 90) {
+    print('Grade: A');
+  } else if (score >= 80) {
+    print('Grade: B');
+  } else if (score >= 70) {
+    print('Grade: C');
+  } else if (score >= 60) {
+    print('Grade: D');
+  } else {
+    print('Grade: F');
+  }
+
+  // Combining Conditions
+  checkDrivingEligibility(10, true);
 
   // switch — clean exact matching
   String status = "success";

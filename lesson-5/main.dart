b0089cc493971fccecf9a5b8596df33267089eff
mdraselmiah95 <russelroy15@gmail.com>
2026-06-15 15:36:00 +0600

    Learing Dart

--- lesson-5/main.dart
+++ lesson-5/main.dart
@@ -5,4 +5,30 @@ void main() {
   for (int i = 0; i < 5; i++) {
     print('Round $i');
   }
+
+  // Task 1: loop
+  List<String> cities=["Dhaka","Chittagong","Khulna","Barishal","Sylhet"];
+  for (int i =0; i<cities.length;i++){
+    print('${i+1}. ${cities[i]}');
+  }
+
+  // Task 2
+  for(int i=1; i<=30;i++){
+    if(i==15) break;
+    if(i%3==0) continue;
+    print(i);
+  }
+
+  // Task 3
+  for(int i=1; i<=15;i++){
+    if(i%3==0 && i%5==0) {
+       print('FizzBuzz');
+    }else if(i%3==0) {
+      print('Fizz');
+    }else if(i%5==0) {
+      print('Buzz');
+    }else {
+      print(i);
+    }
+  }
 }

74baf61b68d3950ec94568821c5b6b2a593f6b43
mdraselmiah95 <russelroy15@gmail.com>
2026-06-23 13:16:32 +0600

    learning dart collection

--- lesson-11/index.dart
+++ lesson-11/index.dart
@@ -1,1 +1,17 @@
 // Error Handling & Async Programming
+
+Future<String> getUserName() {
+  return Future.delayed(Duration(seconds: 2), () {
+    return "Rasel";
+  });
+}
+
+void main() {
+  print("Start");
+
+  getUserName().then((name) {
+    print(name);
+  });
+
+  print("End");
+}

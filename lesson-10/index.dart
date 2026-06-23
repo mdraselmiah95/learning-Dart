5aa841f12a2d9f52b63e4fdc019995d090480f37
mdraselmiah95 <russelroy15@gmail.com>
2026-06-23 12:48:51 +0600

    learning dart collection

--- lesson-10/index.dart
+++ lesson-10/index.dart
@@ -1,22 +1,27 @@
 // Inheritance, Abstract Classes & Mixins
-
 class Animal {
   String name;
+
   Animal(this.name);
 
-  void eat() => print('$name is eating');
+  void eat() {
+    print('$name is eating');
+  }
 }
 
 class Dog extends Animal {
   String breed;
+
   Dog(super.name, this.breed);
 
-  void fetch() => print('$name fetches');
+  void fetch() {
+    print('$name fetches');
+  }
 }
 
 void main() {
-  Dox rex = Dog("Rex", "Beagle");
+  Dog rex = Dog("Rex", "Beagle");
 
   rex.eat();
-  rex.fetch()
+  rex.fetch();
 }
